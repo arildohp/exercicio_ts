a7e7e62522252966f1bc1b74337fd53871901327
arildo <arildohp@gmail.com>
2023-10-08 09:03:46 -0300

    first commit

---
+++ src/exercicio.js
@@ -0,0 +1,11 @@
+"use strict";
+function calcular(a, b) {
+    return a * b;
+}
+;
+const calcula2 = (a, b) => a * b;
+console.log(calcular(2, 5));
+console.log(calcula2(4, 9));
+let nome = 'Arildo';
+let saldacao = `Ola ${nome}.`;
+console.log(saldacao);
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+function calcular(a: number, b: number):number{
+    return a * b
+};
+
+const calcula2 = (a: number, b:number): number => a * b
+
+console.log(calcular(2, 5));
+console.log(calcula2(4,9));
+
+
+let nome:string = 'Arildo';
+let saldacao:string = `Ola ${nome}.`;
+console.log(saldacao);
